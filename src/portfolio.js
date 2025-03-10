d890db8ee5cb3b8b6bf4027d804f9dfd8a4a9029
Reya Singh <khushithakur5848@gmail.com>
2025-03-10 23:15:55 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -395,6 +395,39 @@ const experience = {
 };
 
 // Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects makes use of vast variety of latest technology tools. My best experience is to create Data Science projects and deploy them to web applications using cloud infrastructure.",
+  avatar_image_path: "projects_image.svg",
+};
+
+const publicationsHeader = {
+  title: "Publications",
+  description: "Some of my published Articles, Blogs and Research.",
+  avatar_image_path: "projects_image.svg",
+};
+
+const publications = {
+  data: [
+    {
+      id: "neuro-symbolic-sudoku-solver",
+      name: "Neuro-Symbolic Sudoku Solver",
+      createdAt: "2023-07-02T00:00:00Z",
+      description: "Paper published in KDD KiML 2023",
+      url: "#",
+    },
+    {
+      id: "mdp-diffusion",
+      name: "MDP-Diffusion",
+      createdAt: "2023-09-19T00:00:00Z",
+      description: "Blog published in Paperspace",
+      url: "#",
+    },
+    
+  ],
+};
+
 
 // Contact Page
 const contactPageData = {
@@ -438,6 +471,8 @@ export {
   degrees,
   certifications,
   experience,
-  
+  projectsHeader,
+  publicationsHeader,
+  publications,
   contactPageData,
 };
